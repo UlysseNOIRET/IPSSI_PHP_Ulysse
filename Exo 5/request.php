5a359f9c2fc6590bed3042ae76e4e9a90f6df510
UlysseNOIRET <ulyssenoiret38@gmail.com>
2025-11-19 11:34:23 +0100

    Update request.php

--- Exo 5/request.php
+++ Exo 5/request.php
@@ -1,4 +1,26 @@
 <?php
+require_once("config.php");
 
+$sth = $dbh->prepare("select * from jo.100;");
+$sth->execute();
 
+$data = $sth->fetchAll(PDO::FETCH_ASSOC);
+echo"<table>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Pays</th>
+            <th>Course</th>
+            <th>Temps</th>
+        </tr>
+    </thead>";
+    foreach ($data as $value) {
+        echo "<tr>
+            <td>".$value["nom"]."</td>
+            <td>".$value["pays"]."</td>
+            <td>".$value["course"]."</td>
+            <td>".$value["temps"]."</td>
+        </tr>";
+    }
 ?>
+
