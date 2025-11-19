37bb7f4ff4265ffbb815c233efe69999a5888d73
UlysseNOIRET <ulyssenoiret38@gmail.com>
2025-11-19 16:24:58 +0100

    Update request.php

--- Exo 5 et 6 + Evalution/request.php
+++ Exo 5 et 6 + Evalution/request.php
@@ -18,10 +18,10 @@ $data = $sth->fetchAll(PDO::FETCH_ASSOC);
 echo"<table>
     <thead>
         <tr>
-            <th>Nom<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=nom&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=nom&order=desc'>⬇️</a></th>
-            <th>Pays<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=pays&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=pays&order=desc'>⬇️</a></th>
-            <th>Course<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=course&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=course&order=desc'>⬇️</a></th>
-            <th>Temps<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=temps&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=temps&order=desc'>⬇️</a></th>
+            <th>Nom<a href='./request.php?sort=nom&order=asc'>⬆️</a><a href='./request.php?sort=nom&order=desc'>⬇️</a></th>
+            <th>Pays<a href='./request.php?sort=pays&order=asc'>⬆️</a><a href='./request.php?sort=pays&order=desc'>⬇️</a></th>
+            <th>Course<a href='./request.php?sort=course&order=asc'>⬆️</a><a href='./request.php?sort=course&order=desc'>⬇️</a></th>
+            <th>Temps<a href='./request.php?sort=temps&order=asc'>⬆️</a><a href='./request.php?sort=temps&order=desc'>⬇️</a></th>
         </tr>
     </thead>";
     foreach ($data as $value) {
