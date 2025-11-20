ed4f9887da74b64e8597d9d5af770013c6ded167
UlysseNOIRET <ulyssenoiret38@gmail.com>
2025-11-20 14:53:31 +0100

    Update request.php

--- Exo 8/request.php
+++ Exo 8/request.php
@@ -1,67 +1,87 @@
+<?php
+require_once("config.php");
+
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+ 
+    if ($username != '' && $password != '') {
+        $stmt = $dbh->prepare("SELECT * FROM login.user WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+ 
+        if ($stmt->rowCount() > 0) {
+            $message = "<b>Le nom d'utilisateur existe deja.</b>";
+        } else {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+ 
+            $sth = $dbh->prepare("INSERT INTO login.user (username, password) VALUES (:username, :password)");
+            $sth->execute([
+                'username' => $username,
+                'password' => $hash,
+            ]);
+            $message = "<b>Votre inscription est validee.</b>";
+        }
+    } else {
+        $message = "<b>Veuillez remplir tous les champs.</b>";
+    }
+}
+ 
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+ 
+    if ($username != '' && $password != '') {
+        $stmt = $dbh->prepare("SELECT * FROM login.user WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+ 
+        if ($stmt->rowCount() > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($password, $row['password'])) {
+                $message = "<b>Connexion reussie.</b>";
+            } else {
+                $message = "<b>Le mot de passe est incorrect.</b>";
+            }
+        } else {
+            $message = "<b>Le nom d'utilisateur n'existe pas.</b>";
+        }
+    } else {
+        $message = "<b>Veuillez remplir tous les champs.</b>";
+    }
+}
+?>
+ 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulaire d'inscription et de connexion</title>
 </head>
 <body>
-    <h1>Inscription</h1>
-    <form method="post" action="./">
-        <label>Username : </label>
-        <input type="text" name="username"><br/>
  
-        <label>Password : </label>
-        <input type="password" name="password"><br/>
- 
-        <input type="submit" value="Valider" name="register">
+    <h2>Inscription</h2>
+    <form method="POST">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" name="username" id="username" required>
+       
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" id="password" required>
+       
+        <button type="submit" name="register">S'inscrire</button>
     </form>
  
-    <h1>Connection</h1>
-    <form method="post" action="./">
-        <label>Username : </label>
-        <input type="text" name="username"><br/>
- 
-        <label>Password : </label>
-        <input type="password" name="password"><br/>
- 
-        <input type="submit" value="Valider" name="connect">
+    <h2>Connexion</h2>
+    <form method="POST">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" name="username" id="username" required>
+       
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" id="password" required>
+       
+        <button type="submit" name="login">Se connecter</button>
     </form>
+ 
+    <?php if (isset($message)) { echo "<div>$message</div>"; } ?>
+ 
 </body>
 </html>
- 
-<?php
-if (isset($_POST['register'])) {
-    if ($_POST['username'] != '' && $_POST['password'] != '') {
-        $hash = password_hash($_POST['password'], algo: PASSWORD_DEFAULT);
-        $sth = $dbh->prepare( query: 'INSERT INTO `user` (`username`, `password`) VALUES (:username, :password)');
-        $sth->execute([
-            'username' => $_POST['username'],
-            'password' => $hash,
-        ]);
-        echo "<b>Votre inscription est valide</b>";
-    }
-}
- 
-if (isset($_POST['register'])) {
-    $stmt = $dbh->prepare(query: "SELECT * FROM user WHERE username = :username");
-    $stmt->execute(['username' => $_POST['username']]);
-    $user = $stmt->fetch(mode: PDO::FETCH_ASSOC);
- 
-    if ($user) {
-        if (password_verify($_POST['password'], $user['password'])) {
-            $_SESSION['username'] = $_POST['username'];
-        }
-    }
-}
-if (isset($_POST['connect'])) {
-    $stmt = $dbh->prepare(query: "SELECT * FROM user WHERE username = :username");
-    $stmt->execute(['username' => $_POST['username']]);
-    $user = $stmt->fetch(mode: PDO::FETCH_ASSOC);
- 
-    if ($user) {
-        if (password_verify($_POST['password'], $user['password'])) {
-            $_SESSION['username'] = $_POST['username'];
-        }
-    }
-}
